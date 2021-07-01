2d01bc7d14ea3a33290c0754b3c50037af144dd3
dinud11 <dragomirescudinu@gmail.com>
2021-07-01 12:57:59 +0100

    trying out different designs, nothing working

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,27 @@
 import * as React from 'react'
 import * as componentStyles from './layout.module.css'
 import "./layout.css"
+import { StaticImage } from 'gatsby-plugin-image'
 
 const Layout = ({ children }) => {
     return (
-        <main className={componentStyles.container}>
-            {children}
-        </main>
+        <div>
+            <nav>
+                <ul className={componentStyles.navLinks}>
+                    <li><StaticImage
+                            alt="Logo"
+                            src="../images/icon.png"
+                            width={40}
+                            height={40}
+                        />
+                    </li>
+                    <li className={componentStyles.blogLink}>Blog</li>
+                </ul>
+            </nav>
+            <main className={componentStyles.container}>
+                {children}
+            </main>
+        </div>
     )
 }
 
